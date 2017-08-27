f6a4a5e3f989a4ecf799a4e36715928414f6ad6c
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-27 18:02:44 -0500

    Add files via upload

---
+++ WindowsFormsApp/HelloWorld.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp
+{
+    partial class HelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Location = new System.Drawing.Point(321, 138);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(140, 53);
+            this.btnHelloWorld.TabIndex = 0;
+            this.btnHelloWorld.Text = "HelloWorld";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
+            // HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 411);
+            this.Controls.Add(this.btnHelloWorld);
+            this.Name = "HelloWorld";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHelloWorld;
+    }
+}
+
---
+++ WindowsFormsApp/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp.HelloWorldService;
+
+namespace WindowsFormsApp
+{
+    public partial class HelloWorld : Form
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloWorld_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Run(),"Hello App");
+        }
+
+        public static string Run()
+        {
+            TodaysDataRequest request = new TodaysDataRequest();
+            TodaysDataResponse response = new TodaysDataResponse();
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            response = client.GetTodaysData(request);
+            return response.Message;
+        }
+    }
+}
