aa668f1787491210022bd8090cc97c1e3bc195b6
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-27 17:51:31 -0500

    Add files via upload

---
+++ HelloWorldTest.Tests/HelloWorldConsoleAppUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+
+namespace HelloWorldTest.Tests
+{
+    [TestClass]
+    public class HelloWorldConsoleAppUnitTest
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            const string actualData = "Hello World";
+
+            // Call the method to test
+            string expectedData = MainDriver.Run();
+
+            // Check values
+            Assert.AreEqual(expectedData, actualData);
+
+        }
+    }
+}
---
+++ HelloWorldTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("476fdebd-98aa-4cad-b8c7-7395c0fe1e81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
