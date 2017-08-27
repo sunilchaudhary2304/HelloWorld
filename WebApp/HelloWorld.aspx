d4f9aecad5d717fda1aa7fd338a61a3592ba7775
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-27 17:59:00 -0500

    Add files via upload

---
+++ WebApp/HelloWorld.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HelloWorld.aspx.cs" Inherits="WebApp.HelloWorld" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnHelloWorld" runat="server" Text="HelloWorld" OnClick="btnHelloWorld_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/HelloWorld.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebApp.HelloWorldService;
+
+namespace WebApp
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnHelloWorld_Click(object sender, EventArgs e)
+        {
+          Response.Write(Run());
+        }
+
+        public static string Run()
+        {
+            TodaysDataRequest request = new TodaysDataRequest();
+            TodaysDataResponse response = new TodaysDataResponse();
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            response = client.GetTodaysData(request);
+            return response.Message;
+        }
+    }
+}
