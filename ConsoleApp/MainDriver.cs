83ee942e9e9b2891933595f5400f0ce8178b7ede
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-27 17:53:41 -0500

    Add files via upload

---
+++ ConsoleApp/MainDriver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleApp.HelloWorldService;
+
+namespace ConsoleApp
+{
+  public class MainDriver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Run());
+            Console.ReadKey(); 
+        }
+
+        public static string Run()
+        {
+            TodaysDataRequest request = new TodaysDataRequest();
+            TodaysDataResponse response = new TodaysDataResponse();
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            response = client.GetTodaysData(request);
+            return response.Message;
+            
+        }
+    }
+}
