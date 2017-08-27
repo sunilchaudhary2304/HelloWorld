5f4a415e8ae6046bb0a148d595f4cd5544fa1fdf
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-27 18:04:16 -0500

    Add files via upload

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using WPFApp.HelloWorldService;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloWorld_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Run(), "My App");
+        }
+
+        public static string Run()
+        {
+            TodaysDataRequest request = new TodaysDataRequest();
+            TodaysDataResponse response = new TodaysDataResponse();
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            response = client.GetTodaysData(request);
+            return response.Message;
+        }
+    }
+}
