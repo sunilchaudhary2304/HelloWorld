69ff088ab0351401df93dfbb218ef68b1babc930
Sunil  Chaudhary <sunilchaudhary2304@gmail.com>
2017-08-29 18:58:08 -0500

    Add files via upload

---
+++ WebMVCApp.Tests/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebMVCApp.Controllers;
+
+namespace WebMVCApp.Tests.Controllers
+{
+    [TestClass]
+   public class HelloWorldControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            const string actualData = "Hello World";
+            HelloWorldController controller = new HelloWorldController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            string expectedData = result.ViewBag.Message;
+
+            //Assert
+            Assert.AreEqual(expectedData, actualData);
+        }
+
+    }
+}
